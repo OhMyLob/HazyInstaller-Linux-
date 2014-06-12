c6ba04bea34306baddd31dd7c0b891fb6678023a
OhMyLob <matteo.lobello@programmer.net>
2014-06-12 19:28:13 +0200

    Wget, not urlib.

--- n4.py
+++ n4.py
@@ -1,11 +1,9 @@
-import urllib
 import os
 
 print " "
 os.system("./adb reboot-recovery")
-print "Downloading..."
 #### CHANGE DOWNLOAD LINK ####
-urllib.urlretrieve("http://fs1.d-h.st/download/00113/qWJ/TwixKat_Alpha.zip", "TwixKat_Alpha.zip")
+os.system("wget http://fs1.d-h.st/download")
 print "Done..."
 os.system("./adb reboot-bootloader")
 os.system("./fastboot flash system system.img")
--- n5.py
+++ n5.py
@@ -1,11 +1,9 @@
-import urllib
 import os
 
 print " "
 os.system("./adb reboot-recovery")
-print "Downloading..."
 #### CHANGE DOWNLOAD LINK ####
-urllib.urlretrieve("http://drive.google.com/uc?export=download&confirm=tZlg&id=0B2moooVKpajoR2FoaUJic01TTXM", "TwixKat-alpha1-hammerhead.zip")
+os.system("wget http://drive.google.com/uc?export=download&confirm=tZlg&id=0B2moooVKpajoR2FoaUJic01TTXM")
 print "Done..."
 os.system("./adb reboot-bootloader")
 os.system("./fastboot flash system system.img")
