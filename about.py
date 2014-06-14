506b4b67dd9ceee611462e1013ddd9acb60988ca
OhMyLob <matteo.lobello@programmer.net>
2014-06-14 13:24:40 +0200

    Layout Fixes & About window added

--- about.py
+++ about.py
@@ -8,20 +8,15 @@ class GUI:
     print "Bye!"
 
   def __init__(self):
-    self.gladefile1 = "done.glade"
+    self.gladefile1 = "about.glade"
     self.builder = gtk.Builder()
     self.builder.add_from_file(self.gladefile1)
     self.builder.connect_signals(self)
     self.window1 = self.builder.get_object("window1")
     self.window1.show()
 
-  ### NEXT ###
+  ### EXIT ###
   def on_button1_clicked(self, object, data=None):
-    self.window1.destroy()
-
-  ### ABOUT ###
-  def on_button2_clicked(self, object, data=None):
-    os.system("python about.py")
     self.window1.destroy()
 
     
