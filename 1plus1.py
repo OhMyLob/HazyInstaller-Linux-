865a355b471fd4b414b80cb4ec7cfde2d815b5dd
OhMyLob <matteolob1704@gmail.com>
2014-07-22 17:00:40 +0200

    Fixes

--- 1plus1.py
+++ 1plus1.py
@@ -2,8 +2,28 @@ import os
 
 print " "
 #### CHANGE DOWNLOAD LINKS & NAMES ####
-if os.path.exists("HazyROM.zip", "openrecovery-twrp-2.7.1.0-bacon.img"):
-	os.remove("HazyROM.zip", "openrecovery-twrp-2.7.1.0-bacon.img")
+if os.path.exists("HazyROM.zip"):
+	os.remove("HazyROM.zip")
+	os.system("wget http://fs1.d-h.st/HazyROM.zip")
+	os.system("wget http://techerrata.com/file/twrp2/bacon/openrecovery-twrp-2.7.1.0-bacon.img")
+	print "Done..."
+	print " "
+	print "Rebooting to bootloader..."
+	os.system("./adb reboot-bootloader")
+	print "Booting TWRP..."
+	os.system("./fastboot boot recovery openrecovery*.img")
+	print "Done..."
+	print " "
+	print "Now go to: Advanced -> ADB Sideload"
+	print "Press Enter when you have done this..."
+	waiter = raw_input(" ")
+	print "Flashing Hazy..."
+	os.system("./adb sideload HazyROM.zip")
+	print "Done..."	
+	ex = raw_input("Press enter...")
+	os.system("python done.py")
+elif os.path.exists("openrecovery-twrp-2.7.1.0-bacon.img"):
+	os.remove("openrecovery-twrp-2.7.1.0-bacon.img")
 	os.system("wget http://fs1.d-h.st/HazyROM.zip")
 	os.system("wget http://techerrata.com/file/twrp2/bacon/openrecovery-twrp-2.7.1.0-bacon.img")
 	print "Done..."
