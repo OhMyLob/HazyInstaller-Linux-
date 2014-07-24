33e85eb3141b82c3ae504152347ea0ac960ec8fa
OhMyLob <matteolob1704@gmail.com>
2014-07-24 19:10:05 +0200

    Installation fixes

--- n4.py
+++ n4.py
@@ -3,63 +3,28 @@ import os
 print " "
 #### CHANGE DOWNLOAD LINK ####
 if os.path.exists("HazyROM.zip"):
-	os.remove("HazyROM.zip", "openrecovery-twrp-2.7.1.0-mako.img")
-	os.system("wget http://fs1.d-h.st/HazyROM.zip")
-	os.system("wget http://techerrata.com/file/twrp2/mako/openrecovery-twrp-2.7.1.0-mako.img")
-	print "Done..."
-	print " "
-	print "Rebooting to bootloader..."
-	os.system("./adb reboot-bootloader")
-	print "Booting TWRP..."
-	os.system("./fastboot boot recovery openrecovery*.img")
-	print "Done..."
-	print " "
-	print "Now go to: Advanced -> ADB Sideload"
-	print "Press Enter when you have done this..."
-	waiter = raw_input(" ")
-	print "Flashing Hazy..."
-	os.system("./adb sideload HazyROM.zip")
-	print "Done..."	
-	ex = raw_input("Press enter...")
-	os.system("python done.py")
-elif os.path.exists("openrecovery-twrp-2.7.1.0-mako.img"):
+	os.remove("HazyROM.zip")
+	
+if os.path.exists("openrecovery-twrp-2.7.1.0-mako.img"):
 	os.remove("openrecovery-twrp-2.7.1.0-mako.img")
-	os.system("wget http://fs1.d-h.st/HazyROM.zip")
-	os.system("wget http://techerrata.com/file/twrp2/mako/openrecovery-twrp-2.7.1.0-mako.img")
-	print "Done..."
-	print " "
-	print "Rebooting to bootloader..."
-	os.system("./adb reboot-bootloader")
-	print "Booting TWRP..."
-	os.system("./fastboot boot recovery openrecovery*.img")
-	print "Done..."
-	print " "
-	print "Now go to: Advanced -> ADB Sideload"
-	print "Press Enter when you have done this..."
-	waiter = raw_input(" ")
-	print "Flashing Hazy..."
-	os.system("./adb sideload HazyROM.zip")
-	print "Done..."	
-	ex = raw_input("Press enter...")
-	os.system("python done.py")
+	 
+os.system("wget http://fs1.d-h.st/HazyROM.zip")
+os.system("wget http://techerrata.com/file/twrp2/mako/openrecovery-twrp-2.7.1.0-mako.img")
+print "Done..."
+print " "
+print "Rebooting to bootloader..."
+os.system("./adb reboot-bootloader")
+print "Booting TWRP..."
+os.system("./fastboot boot recovery openrecovery-twrp-2.7.1.0-mako.img")
+print "Done..."
+print " "
+print "Now go to: Advanced -> ADB Sideload"	
+print "Press Enter when you have done this..."
+waiter = raw_input(" ")
+print "Flashing Hazy..."
+os.system("./adb sideload HazyROM.zip")
+print "Done..."	
+ex = raw_input("Press enter...")		
+os.system("python done.py")
 
-else:   
-	os.system("wget http://fs1.d-h.st/HazyROM.zip")
-	os.system("wget http://techerrata.com/file/twrp2/mako/openrecovery-twrp-2.7.1.0-mako.img")
-	print "Done..."
-	print " "
-	print "Rebooting to bootloader..."
-	os.system("./adb reboot-bootloader")
-	print "Booting TWRP..."
-	os.system("./fastboot boot recovery openrecovery*.img")
-	print "Done..."
-	print " "
-	print "Now go to: Advanced -> ADB Sideload"	
-	print "Press Enter when you have done this..."
-	waiter = raw_input(" ")
-	print "Flashing Hazy..."
-	os.system("./adb sideload HazyROM.zip")
-	print "Done..."	
-	ex = raw_input("Press enter...")		
-	os.system("python done.py")
 
--- n5.py
+++ n5.py
@@ -4,62 +4,26 @@ print " "
 #### CHANGE DOWNLOAD LINK ####
 if os.path.exists("HazyROM.zip"):
 	os.remove("HazyROM.zip")
-	os.system("wget http://fs1.d-h.st/HazyROM.zip")
-	os.system("wget http://techerrata.com/file/twrp2/hammerhead/openrecovery-twrp-2.7.1.0-hammerhead.img")
-	print "Done..."
-	print " "
-	print "Rebooting to bootloader..."
-	os.system("./adb reboot-bootloader")
-	print "Booting TWRP..."
-	os.system("./fastboot boot recovery openrecovery*.img")
-	print "Done..."
-	print " "
-	print "Now go to: Advanced -> ADB Sideload"
-	print "Press Enter when you have done this..."
-	waiter = raw_input(" ")
-	print "Flashing Hazy..."
-	os.system("./adb sideload HazyROM.zip")
-	print "Done..."	
-	ex = raw_input("Press enter...")
-	os.system("python done.py")
-elif os.path.exists("openrecovery-twrp-2.7.1.0-hammerhead.img"):
-	os.remove("HazyROM.zip", "openrecovery-twrp-2.7.1.0-hammerhead.img")
-	os.system("wget http://fs1.d-h.st/HazyROM.zip")
-	os.system("wget http://techerrata.com/file/twrp2/hammerhead/openrecovery-twrp-2.7.1.0-hammerhead.img")
-	print "Done..."
-	print " "
-	print "Rebooting to bootloader..."
-	os.system("./adb reboot-bootloader")
-	print "Booting TWRP..."
-	os.system("./fastboot boot recovery openrecovery*.img")
-	print "Done..."
-	print " "
-	print "Now go to: Advanced -> ADB Sideload"
-	print "Press Enter when you have done this..."
-	waiter = raw_input(" ")
-	print "Flashing Hazy..."
-	os.system("./adb sideload HazyROM.zip")
-	print "Done..."	
-	ex = raw_input("Press enter...")
-	os.system("python done.py")
 
-else:   
-	os.system("wget http://fs1.d-h.st/HazyROM.zip")
-	os.system("wget http://techerrata.com/file/twrp2/hammerhead/openrecovery-twrp-2.7.1.0-hammerhead.img")
-	print "Done..."
-	print " "
-	print "Rebooting to bootloader..."
-	os.system("./adb reboot-bootloader")
-	print "Booting TWRP..."
-	os.system("./fastboot boot recovery openrecovery*.img")
-	print "Done..."
-	print " "
-	print "Now go to: Advanced -> ADB Sideload"	
-	print "Press Enter when you have done this..."
-	waiter = raw_input(" ")
-	print "Flashing Hazy..."
-	os.system("./adb sideload HazyROM.zip")
-	print "Done..."	
-	ex = raw_input("Press enter...")		
-	os.system("python done.py")
+if os.path.exists("openrecovery-twrp-2.7.1.0-hammerhead.img"):
+	os.remove("openrecovery-twrp-2.7.1.0-hammerhead.img")
+  
+os.system("wget http://fs1.d-h.st/HazyROM.zip")
+os.system("wget http://techerrata.com/file/twrp2/hammerhead/openrecovery-twrp-2.7.1.0-hammerhead.img")
+print "Done..."
+print " "
+print "Rebooting to bootloader..."
+os.system("./adb reboot-bootloader")
+print "Booting TWRP..."
+os.system("./fastboot boot recovery openrecovery*.img")
+print "Done..."
+print " "
+print "Now go to: Advanced -> ADB Sideload"	
+print "Press Enter when you have done this..."
+waiter = raw_input(" ")
+print "Flashing Hazy..."
+os.system("./adb sideload HazyROM.zip")
+print "Done..."	
+x = raw_input("Press enter...")		
+os.system("python done.py")
 
